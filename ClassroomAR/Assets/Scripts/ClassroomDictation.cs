85e3f96600723fe70f0bbe80e06b612c9d3327f7
DanSalib <dansalib@outlook.com>
2018-05-05 00:43:32 -0400

    setup vuforia AR library and Dictation

---
+++ ClassroomAR/Assets/Scripts/ClassroomDictation.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Windows.Speech;
+
+public class ClassroomDictation : MonoBehaviour {
+
+    [SerializeField]
+    private Text m_Hypotheses;
+
+    [SerializeField]
+    private Text m_Recognitions;
+
+    private DictationRecognizer m_DictationRecognizer;
+
+    void Start()
+    {
+        m_DictationRecognizer = new DictationRecognizer();
+
+        m_DictationRecognizer.DictationResult += (text, confidence) =>
+        {
+            Debug.LogFormat("Dictation result: {0}", text);
+            m_Recognitions.text += text + "\n";
+        };
+
+        m_DictationRecognizer.DictationHypothesis += (text) =>
+        {
+            Debug.LogFormat("Dictation hypothesis: {0}", text);
+            m_Hypotheses.text += text;
+        };
+
+        m_DictationRecognizer.DictationComplete += (completionCause) =>
+        {
+            if (completionCause != DictationCompletionCause.Complete)
+                Debug.LogErrorFormat("Dictation completed unsuccessfully: {0}.", completionCause);
+        };
+
+        m_DictationRecognizer.DictationError += (error, hresult) =>
+        {
+            Debug.LogErrorFormat("Dictation error: {0}; HResult = {1}.", error, hresult);
+        };
+
+        m_DictationRecognizer.Start();
+    }
+}
