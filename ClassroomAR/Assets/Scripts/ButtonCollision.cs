493a423fcd5c7f8ecb4eb396bab914851cafb797
DanSalib <dansalib@outlook.com>
2018-05-06 23:39:15 -0400

    added better collision detection

--- ClassroomAR/Assets/Scripts/ButtonCollision.cs
+++ ClassroomAR/Assets/Scripts/ButtonCollision.cs
@@ -6,7 +6,6 @@ public class ButtonCollision : MonoBehaviour
 {
     public void OnTriggerEnter(Collider other)
     {
-        UnityEngine.Debug.Log("ENTER");
         Button collidedButton = other.gameObject.GetComponent<Button>();
         collidedButton.onClick.Invoke();
     }
